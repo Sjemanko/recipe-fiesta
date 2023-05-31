84f74b61b53e46d7b5ce2dd3513c045a1fcd5b45
Sjemanko <tomekbu.ecki@gmail.com>
2023-05-31 19:02:37 +0200

    Changed usermodel

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -4,12 +4,7 @@ import { Request, Response } from "express";
 const prisma = new PrismaClient();
 
 const testFunc = async (req: Request, res: Response) => {
-  const allUsers = await prisma.user.findMany({
-    include: {
-      posts: true,
-      profile: true,
-    },
-  });
+  const allUsers = await prisma.user.findMany();
   return res.status(200).json({ message: "teste", body: allUsers});
 };
 
